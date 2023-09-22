86301f27368505b1bd24115a75f1cf2550da9f76
lion48z <140863653+lion48z@users.noreply.github.com>
2023-09-21 21:13:56 -0500

    Update destructuring.js

--- destructuring.js
+++ destructuring.js
@@ -3,34 +3,57 @@ const [firstFruit, secondFruit, thirdFruit, fourthFruit, fifthFruit] = fruits;
 //console.log(firstFruit);
 //console.log(fourthFruit);
 
-const person = {
+/*const person = {
     firstName: "John",
     lastName: "Doe",
     age: 30
 };
 
-const {firstName: first, lastName: last, age: age} = person;
+const {firstName: first, lastName: last, age: age} = person;*/
 
 //console.log(last);
 
 // simulating API data .json
-const user = {
+/*const user = {
     id: 1,
     name: "John Doe",
     address: {
         street:'123 Main St',
         city: "Anytown"
     }
-};
+};*/
 
 /*console.log(user.name);
 console.log(user.address.street);*/
 
-const {
+/*const {
     name,
     address: {street, city }
-} =user;
+} =user;*/
 
-console.log(name);
+/*console.log(name);
 console.log(street);
-console.log(city);
+console.log(city);*/
+
+const rgb = [255, 200, 0];
+
+const [r, g, b] =rgb
+
+//console.log(`Red: ${r}, Green: ${g}, Blue: ${b}`);
+
+const student = {
+    name: 'Jane Doe',
+    age: 25,
+    scores: {
+        mathematics: 85,
+        english: 90
+    }
+};
+
+const {
+    name,
+    age,
+    scores: {mathematics, english
+}
+} =student;
+console.log(`Name: ${name}, Age: ${age}, Mathematics Score: ${mathematics}, English Score: ${english}`);
