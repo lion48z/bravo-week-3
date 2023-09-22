1a6b1a0f96e0dd05605f4772fd0009b45c346028
lion48z <140863653+lion48z@users.noreply.github.com>
2023-09-22 13:45:02 -0500

    have to write this

---
+++ exercise6hw.js
@@ -0,0 +1,25 @@
+//Exercise 6
+//Given an array of produce objects, use the filter method to create a new 
+//array containing only the ‘Fruit’ type products that have a quantity less than 10.
+
+const produce = [
+  { name: 'Apple', type: 'Fruit', quantity: 10 },
+  { name: 'Carrot', type: 'Vegetable', quantity: 20 },
+  { name: 'Orange', type: 'Fruit', quantity: 5 },
+  { name: 'Celery', type: 'Vegetable', quantity: 15 },
+  { name: 'Banana', type: 'Fruit', quantity: 2 },
+  { name: 'Spinach', type: 'Vegetable', quantity: 30 },
+  { name: 'Grapes', type: 'Fruit', quantity: 8 },
+  { name: 'Lettuce', type: 'Vegetable', quantity: 5 },
+];
+
+
+
+//Expected Output:
+/*
+[
+  { name: 'Orange', type: 'Fruit', quantity: 5 },
+  { name: 'Banana', type: 'Fruit', quantity: 2 },
+  { name: 'Grapes', type: 'Fruit', quantity: 8 }
+]
+*/
